9d60f2653a69bd9ea300f828e14d18064ab46e7d
Wyatt <wwerner@shortridgeacademy.com>
2017-02-10 15:27:24 -0500

    a commit for steve

--- chap2.py
+++ chap2.py
@@ -2,3 +2,37 @@ print ("hello coding world")
 print (8 * 3.57)
 print (10 * 365)
 print (20 + 3650)
+print (3 * 52)
+print (3670 - 156)
+print (5 + 30 * 20)
+print ((5 + 30)* 20)
+print (((5 + 30)*20)/ 10)
+print (5 + 30 * 20 / 10)
+fred = 100
+print (fred)
+fred = 200
+print (fred)
+john = fred
+print (john)
+number_of_coins = 200
+print (number_of_coins)
+print (20 + 10 * 365)
+print (3 * 52)
+print (3670 - 156)
+print (20 + 10 * 365 - 3 * 52)
+found_coins = 20
+magic_coins = 10
+stolen_coins = 3
+print (found_coins + magic_coins * 365 - stolen_coins * 52)
+stolen_coins = 2
+print (found_coins + magic_coins * 365 - stolen_coins * 52)
+magic_coins = 13
+print (found_coins + magic_coins * 365 - stolen_coins * 52)
+jack = "Why do gorillas have big nostrils? Big Fingers!!"
+print (jack)
+jack = 'What is pink and fluffy? Pink fluff!!'
+print (jack)
+jack = '''How do dinosaurs pay their bills?
+With tyrannosaurus checks!'''
+print (jack)
+silly_string = '''He said, "Aren't can't shouldn't wouldn't."'''
